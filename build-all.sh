0d518f70d4c0dda62d59477f59b025406346d1f3
Ata <quetzalskeith@gmail.com>
2021-06-21 05:09:59 +0700

    Update build & push

--- build-all.sh
+++ build-all.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+./php-5.6-fpm/build.sh
 ./php-5.6-fpm-alpine/build.sh
 ./php-7.3-cli-alpine/build.sh
+./php-7.3-fpm/build.sh
 ./php-7.3-fpm-alpine/build.sh
 ./php-7.3-worker/build.sh
--- push-all.sh
+++ push-all.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+docker push quetzalarc/php:5.6-fpm
 docker push quetzalarc/php:5.6-fpm-alpine
 docker push quetzalarc/php:7.3-cli-alpine
+docker push quetzalarc/php:7.3-fpm
 docker push quetzalarc/php:7.3-fpm-alpine
 docker push quetzalarc/php:7.3-worker
